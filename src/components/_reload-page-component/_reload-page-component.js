cfff70922731feffe9754175d4144d40ebdf3dfc
Nick Khitrov <deliriumbred@yandex.ru>
2021-06-03 13:12:40 +0300

    Change script of reload-page component for encapsulate. Wraped it in IIFE. Also add necessary function for format time when a page reload

--- src/components/_reload-page-component/_reload-page-component.js
+++ src/components/_reload-page-component/_reload-page-component.js
@@ -1,54 +1,67 @@
+(function() {
+  const reloadPageComponentList = document.querySelectorAll('.reload-page-component');
 
-const reloadPageComponentList = document.querySelectorAll('.reload-page-component');
-
-const addNewTimeTimer = (to, newTime) => {
-  to.innerHTML = newTime;
-};
-
-const translateDateToNumber = (date) => {
-  let minute, second;
-    
-    minute = Number(date.slice(0, 2));
-    second = Number(date.slice(3));
-
-  return {
-    minute,
-    second,
+  const addNewTimeTimer = (to, newTime) => {
+    to.innerText = newTime;
   };
-};
-// Функция принимает числа, обрабатывает их и возвращает строку для дальнейшего отображения в DOM узле
-const subtractionTimer = (min, sec) => {
-  let resultMinute, resultSecond;
-  // Проверка, что вычитать - минутный или секундный параметр
-  // Если секунда - это 00, то вычитать минуту и ставить секунды в 59
-  if (sec !== 0) {
-    (sec <= 10) ? resultSecond = `0${sec -= 1}` : resultSecond = `${sec -= 1}`;
-    (min <= 9) ? resultMinute = `0${min}` : resultMinute = min;
-  } else {
-    resultSecond = 59;
-    (min <= 10) ? resultMinute = `0${min -= 1}` : resultMinute = `${min -= 1}`;
-  }
-  return `${resultMinute}:${resultSecond}`
-};
 
-reloadPageComponentList.forEach(item => {
-  const itemBtn = item.querySelector('.reload-page-component__btn');
-  const timer = item.querySelector('.reload-page-component__date-span');
+  const translateDateToNumber = (date) => {
+    let minute, second;
 
-  timer.innerText = item.getAttribute('data-reload-page-timer');
-  timer.setAttribute('datetime', item.getAttribute('data-reload-page-timer'));
+      const dateArr = date.split(':');
 
-  itemBtn.addEventListener('click', (e) => {
-    itemBtn.disabled = true;
+      minute = Number(dateArr[0].trim());
+      second = Number(dateArr[1].trim());
 
-    setInterval(() => {
-      // Положил в переменные данные с типом Number для передачи их в функцию substractionTimer
-      let minute = translateDateToNumber(timer.innerText).minute;
-      let second = translateDateToNumber(timer.innerText).second;
+    return {
+      minute,
+      second,
+    };
+  };
+  // Функция принимает числа, обрабатывает их и возвращает строку для дальнейшего отображения в DOM узле
+  const subtractionTimer = (min, sec) => {
+    let resultMinute, resultSecond;
+    // Проверка, что вычитать - минутный или секундный параметр
+    // Если секунда - это 00, то вычитать минуту и ставить секунды в 59
+    if (sec !== 0) {
+      (sec <= 10) ? resultSecond = `0${sec -= 1}` : resultSecond = `${sec -= 1}`;
+      (min <= 9) ? resultMinute = `0${min}` : resultMinute = min;
+    } else {
+      resultSecond = 59;
+      (min <= 10) ? resultMinute = `0${min -= 1}` : resultMinute = `${min -= 1}`;
+    }
+    return `${resultMinute} : ${resultSecond}`
+  };
 
-      if (minute === 0 && second === 1) clearInterval(window.location.reload());
+  reloadPageComponentList.forEach(item => {
+    const itemBtn = item.querySelector('.reload-page-component__btn');
+    const timer = item.querySelector('.reload-page-component__date-span');
+    const thisTimerTime = item.getAttribute('data-reload-page-timer');
+    // Функция которая задаёт первое значение таймера при загрузки страницы в нужном формате
+    const setTimerTime = () => {
+      const dateArr = thisTimerTime.split(':');
 
-      addNewTimeTimer(timer, subtractionTimer(minute, second))
-    }, 1000)
-  })
-});
+      const minute = dateArr[0].trim();
+      const second = dateArr[1].trim();
+
+      return `${minute} : ${second}`;
+    };
+    // Добавление значений таймера и аттрибута в тег time из data аттрибута
+    addNewTimeTimer(timer, setTimerTime());
+    timer.setAttribute('datetime', thisTimerTime);
+
+    itemBtn.addEventListener('click', (e) => {
+      itemBtn.disabled = true;
+
+      setInterval(() => {
+        // Положил в переменные данные с типом Number для передачи их в функцию substractionTimer
+        let minute = translateDateToNumber(timer.innerText).minute;
+        let second = translateDateToNumber(timer.innerText).second;
+        // Если время становится в 00 : 01 - выполнить перезагрузку страницы
+        if (minute === 0 && second === 1) clearInterval(window.location.reload());
+        // Иначе запускать функцию вычитания времени и рендерить новое значение
+        addNewTimeTimer(timer, subtractionTimer(minute, second))
+      }, 1000);
+    });
+  });
+})();
