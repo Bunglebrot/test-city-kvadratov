933e2c2bc0d1a084766a805bc11cbb32742b3c3f
Nick Khitrov <deliriumbred@yandex.ru>
2021-06-03 13:07:16 +0300

    Remove forEach polyfill and add core-js script to the index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-
+import 'core-js/stable';
 import './index.html';
 import './main.scss';
-import './js/forEachPolyfill';
 import './components/_reload-page-component/_reload-page-component.js';
+import './components/_repair-work-block/_repair-work-block';
